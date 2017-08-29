8fa779455a7585d78c59547cd131daab172e138f
Alexandre Debert <alexandre10.debert@gmail.com>
2017-08-29 07:39:34 -0300

    Creating Select component

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -1,35 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Fieldset from '../fieldset/fieldset';
+import Select from '../select/select';
 
 import './loginForm.scss';
 
 
-const LoginForm = () => (
-    <div className="form-container">
-        <form >
-            <h1>Login</h1>
-            <Fieldset 
-                htmlFor={'user-name'}
-                label={'Username'}
-                inputId={'user-name'}
-                inputType={'text'}
-                placeholder={'Username'}
-            />
-            <Fieldset 
-                htmlFor={'user-password'}
-                label={'Password'}
-                inputId={'user-password'}
-                inputType={'text'}
-                placeholder={'Password'}
-            />
-            <select id="user-airline">
-                <option value="LATAM">LATAM</option>
-                <option value="AA">AA</option>
-                <option value="Air France">Air France</option>
-            </select><br/>
-            <button>SUBMIT</button>
-        </form>
-    </div>
-);
+class LoginForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            airlineOptions: []
+        };
+        this.handleAirlineSelect = this.handleAirlineSelect.bind(this);
+        //this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('https://beta.id90travel.com/airlines')
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    airlineOptions: data.display_name
+                });
+            });
+    }
+
+    handleAirlineSelect(e) {
+        this.setState({ ownerAgeRangeSelection: e.target.value });
+    }
+
+    render() {
+        return (
+            <div className="form-container">
+                <form >
+                    <h1>Login</h1>
+                    <Fieldset
+                        htmlFor={'user-name'}
+                        label={'Username'}
+                        inputId={'user-name'}
+                        inputType={'text'}
+                        placeholder={'Insert your username'}/>
+                    <Fieldset
+                        htmlFor={'user-password'}
+                        label={'Password'}
+                        inputId={'user-password'}
+                        inputType={'text'}
+                        placeholder={'Insert your password'}/>
+                    <Select
+                        name={'airline'}
+                        placeholder={'Choose your Airline'}
+                        controlFunc={this.handleAirlineSelect}
+                        options={this.state.airlineOptions}
+                        selectedOption={this.state.ownerAgeRangeSelection} />
+                    <button>SUBMIT</button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default LoginForm;
